33b17008ba972e6acbb32cef4017540aebb4128b
Tom Totenberg <ttotenberg@launchdarkly.com>
2024-12-12 14:44:17 -0600

    removed inExperiment detection

--- main.py
+++ main.py
@@ -55,23 +55,52 @@ def error_chance(chance_number):
         return False
 
 '''
-Evaluate the flag for randomly generated users, and make the track() calls to LaunchDarkly
+Evaluate the flag for randomly generated users, and make the track() calls to LaunchDarkly. The commented out method no longer works, since the overall flag doesn't have an 'inExperiment' value in the variationDetail result. Instead, just brute forcing lots of events to LD.
 '''
+# def callLD():
+
+#     context = create_multi_context()
+#     flag_detail = ldclient.get().variation_detail(
+#         FLAG_KEY, 
+#         context, 
+#         'unavailable'
+#     )
+
+#     in_experiment = flag_detail.reason.get('inExperiment')
+#     if in_experiment is None:
+#         print("Release Guardian is not running. Retrying in 5 seconds.")
+
+#     if in_experiment:
+#         for i in range(1000):
+#             context = create_multi_context()
+#             flag_variation = ldclient.get().variation(FLAG_KEY, context, False)
+#             if flag_variation:
+#                 print("Executing " + str(flag_variation))
+#                 if error_chance(int(BINARY_METRIC_1_TRUE_CONVERTED)):
+#                     ldclient.get().track(BINARY_METRIC_1, context)
+#                     print("Tracking " + BINARY_METRIC_1)
+#                 else:
+#                     numeric_metric_value = random.randint(int(NUMERIC_METRIC_1_TRUE_RANGE[0]), int(NUMERIC_METRIC_1_TRUE_RANGE[1]))
+#                     ldclient.get().track(NUMERIC_METRIC_1, context, metric_value=numeric_metric_value)
+#                     print(f"Tracking {NUMERIC_METRIC_1} with value {numeric_metric_value}")
+
+#             else:
+#                 print("Executing " + str(flag_variation))
+#                 if error_chance(int(BINARY_METRIC_1_FALSE_CONVERTED)):
+#                     ldclient.get().track(BINARY_METRIC_1, context)
+#                     print("Tracking " + BINARY_METRIC_1)
+#                 else:
+#                     numeric_metric_value = random.randint(int(NUMERIC_METRIC_1_FALSE_RANGE[0]), int(NUMERIC_METRIC_1_FALSE_RANGE[1]))
+#                     ldclient.get().track(NUMERIC_METRIC_1, context, metric_value=numeric_metric_value)
+#                     print(f"Tracking {NUMERIC_METRIC_1} with value {numeric_metric_value}")
+            
+#             ldclient.get().flush()
+#             time.sleep(.05)
+#     else:
+#         time.sleep(5)
+
 def callLD():
-
-    context = create_multi_context()
-    flag_detail = ldclient.get().variation_detail(
-        FLAG_KEY, 
-        context, 
-        'unavailable'
-    )
-
-    in_experiment = flag_detail.reason.get('inExperiment')
-    if in_experiment is None:
-        print("Release Guardian is not running. Retrying in 5 seconds.")
-
-    if in_experiment:
-        for i in range(1000):
+    for i in range(1000):
             context = create_multi_context()
             flag_variation = ldclient.get().variation(FLAG_KEY, context, False)
             if flag_variation:
@@ -96,9 +125,6 @@ def callLD():
             
             ldclient.get().flush()
             time.sleep(.05)
-    else:
-        time.sleep(5)
-
 
 '''
 Execute! Push Ctrl+C to stop the loop.
